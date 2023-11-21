86c3c70da3e6e5ac5649665d11c5d4c86fcb3843
Binary12Spy <134235374+Binary12Spy@users.noreply.github.com>
2023-11-20 20:30:13 -0500

    Updated ps1 script

--- winget-turbo.ps1
+++ winget-turbo.ps1
@@ -1,11 +1,32 @@
-# Check if running as administrator
-$isElevated = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-
-if (-not $isElevated) {
-    # Relaunch script with elevated permissions
-    Start-Process -FilePath "powershell.exe" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File $($MyInvocation.MyCommand.Path)" -Verb RunAs
-    exit
+function Test-IsAdministrator {
+    $Identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $Principal = New-Object System.Security.Principal.WindowsPrincipal($Identity)
+    $Principal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
 }
+
+function Test-IsUacEnabled {
+    (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System).EnableLua -ne 0
+}
+
+function Request-AdminRights {
+    # Check if already an administrator
+    if (!(Test-IsAdministrator)) {
+        if (Test-IsUacEnabled) {
+            # Construct argument list
+            [string[]]$argList = @('-NoProfile', '-NoExit', '-File', $MyInvocation.MyCommand.Path)
+            $argList += $MyInvocation.BoundParameters.GetEnumerator() | ForEach-Object { "-$($_.Key)", "$($_.Value)" }
+            $argList += $MyInvocation.UnboundArguments
+
+            # Start new elevated process
+            Start-Process PowerShell.exe -Verb Runas -WorkingDirectory $pwd
+            return
+        } else {
+            throw "You must be an administrator to run this script."
+        }
+    }
+}
+
+Request-AdminRights
 
 # Read file, filtering out blank lines
 $inputFile = ".\packages.txt"
@@ -16,5 +37,5 @@ $filteredLines = $lines | Where-Object { $_ -notmatch '^#' }
 
 foreach ($line in $filteredLines) {
     # Execute winget command
-    Start-Process -FilePath "cmd.exe" -ArgumentList "/c winget install --id=$line -e" -Verb RunAs
+    Start-Process PowerShell -ArgumentList "-Command", "& {winget install --id=$line -e}"
 }
